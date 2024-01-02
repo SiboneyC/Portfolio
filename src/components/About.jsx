56de23a9e38b2941a3fa5fd80eda3013de9235b1
RikGhosh487 <rikghosh487@gmail.com>
2024-01-02 17:20:31 -0600

    Added modals

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,10 @@
 import PropTypes from "prop-types";
+import { Fragment, useState } from "react";
 
 function About({ interests, recents }) {
+  const [modalActive, setModalActive] = useState(false);
+  const [modalInfo, setModalInfo] = useState(undefined);
+
   return (
     <article className="about active">
       <header>
@@ -53,29 +57,74 @@ function About({ interests, recents }) {
       )}
 
       {Array.isArray(recents) && recents.length && (
-        <section className="recents">
-          <h3 className="h3 recents-title">Recent Projects</h3>
-          <ul className="recents-list has-scrollbar">
-            {recents.map((rec, idx) => (
-              <li
-                className="recents-item"
-                key={rec.toString() + idx.toString()}
-              >
-                <div className="content-card">
-                  <figure className="recents-avatar-box">
-                    <img src={rec["img"]} alt={rec["alt"]} width={60} />
-                  </figure>
-
-                  <h4 className="h4 recents-item-title">{rec["name"]}</h4>
-
-                  <div className="recents-text">
-                    <p>{rec["small_info"]}</p>
+        <Fragment>
+          <section className="recents">
+            <h3 className="h3 recents-title">Recent Projects</h3>
+            <ul className="recents-list has-scrollbar">
+              {recents.map((rec, idx) => (
+                <li
+                  className="recents-item"
+                  key={rec.toString() + idx.toString()}
+                >
+                  <div
+                    className="content-card"
+                    onClick={() => {
+                      setModalActive(true);
+                      setModalInfo(rec);
+                    }}
+                  >
+                    <figure className="recents-avatar-box">
+                      <img src={rec["img"]} alt={rec["alt"]} width={60} />
+                    </figure>
+                    <h4 className="h4 recents-item-title">{rec["name"]}</h4>
+                    <div className="recents-text">
+                      <p>{rec["small_info"]}</p>
+                    </div>
                   </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </section>
+                </li>
+              ))}
+            </ul>
+          </section>
+          <div className={`modal-container ${modalActive ? "active" : ""}`}>
+            <div className="overlay"></div>
+            <div className="projects-modal">
+              <button
+                className="modal-close-btn"
+                onClick={() => {
+                  setModalActive(false);
+                  setModalInfo(undefined);
+                }}
+              >
+                <ion-icon name="close-outline"></ion-icon>
+              </button>
+
+              {modalInfo !== undefined && (
+                <Fragment>
+                  <div className="modal-img-wrapper">
+                    <figure className="modal-avatar-box">
+                      <img src={modalInfo["img"]} alt={modalInfo["alt"]} />
+                    </figure>
+                  </div>
+                  <div className="modal-content">
+                    <h4 className="h3 modal-title">{modalInfo["name"]}</h4>
+                    <time dateTime={modalInfo["date"]}>
+                      {modalInfo["date"]}
+                    </time>
+
+                    <div className="data-model-text">
+                      <p className="data-desc">{modalInfo["desc"]}</p>
+                      <p>{modalInfo["small_info"]}</p>
+                      <p className="read-more">
+                        To read more about this project, visit the Projects
+                        page!
+                      </p>
+                    </div>
+                  </div>
+                </Fragment>
+              )}
+            </div>
+          </div>
+        </Fragment>
       )}
     </article>
   );
